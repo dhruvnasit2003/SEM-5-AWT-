012a46ed9e93a44b8a8dfe499a66c2088aa508f4
dhruvnasit2003 <110342953+dhruvnasit2003@users.noreply.github.com>
2023-07-29 21:19:51 +0530

    Add files via upload

---
+++ Practical List/Practical-1/pr1.js
@@ -0,0 +1,11 @@
+const StringName ="Dhruv" 
+const Roll = 79
+const Admission = true
+console.log(StringName); 
+console.log(Roll); 
+console.log(Admission);
+console.log("Running the function"); 
+const fun = (a,b) => {
+return a + b
+}
+console.log(fun(2,3));
